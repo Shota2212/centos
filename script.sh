6c867097bd36c467bb60f0c074e85f98467548b0
Shota_22 <84522061+Shota2212@users.noreply.github.com>
2023-12-12 00:16:57 +0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,7 +8,7 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo
 sudo yum check-update
 sudo yum install code -y
-sudo yum install chromium
+sudo yum install chromium -y
 sudo systemctl isolate graphical.target
 sudo systemctl set-default graphical.target
 rm '/etc/systemd/system/default.target'
