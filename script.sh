257ca266bcbe5545a52bcd9756b9f3c063bf97c7
Shota_22 <84522061+Shota2212@users.noreply.github.com>
2023-12-11 23:49:51 +0400

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo yum -y install epel-release
+sudo yum -y groupinstall "X Window system"
+sudo yum -y install lightdm
+sudo yum -y install cinnamon
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo
+sudo yum check-update
+sudo yum install code -y
+sudo yum install chromium
+sudo systemctl isolate graphical.target
+sudo systemctl set-default graphical.target
+rm '/etc/systemd/system/default.target'
+ln -s '/usr/lib/systemd/system/graphical.target' '/etc/systemd/system/default.target'
